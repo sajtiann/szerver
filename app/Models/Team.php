d2c3716ddce2bb6ac9fbf08e6d454b1185772712
sajtiann <stannacska@gmail.com>
2023-03-25 20:21:08 +0100

    n:n connection

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -20,4 +20,8 @@ class Team extends Model {
         return $this->belongsTo(Game::class, 'away_team_id');
     }
 
+    public function users() {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+
 }
